1d36070aca75eb5da855f846e37772c18e38c6d2
BSM-777 <marouanebassim12@gmail.com>
2024-02-19 09:59:29 +0100

    New

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 /**
  * bubble_sort - sorts an array of integers using bubble sort
---
+++ sort.h
@@ -0,0 +1,17 @@
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void swap_values(int **array, ssize_t i1, ssize_t i2);
